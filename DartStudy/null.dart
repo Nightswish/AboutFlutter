5d4718632b7e55d9a08e7ea7c69dd7e73f43351d
unknown <luvrkdus@gmail.com>
2024-07-16 13:49:15 +0900

    Start Study Dart

---
+++ DartStudy/null.dart
@@ -0,0 +1,17 @@
+void main() {
+  // nullable - null이 될 수 있다.
+  // non-nullable - null이 될수 없다
+
+  String name = 'check'; // null이 들어갈 수 없다.
+
+  print(name);
+
+  // name = null; 안됨
+
+  String? name2 = 'blackpink'; // 타입뒤에 ? 하면 null 까지 가능  
+
+  name2 = null;
+
+  print(name2!); // null이 절대로 들어갈 수 없다. 즉 null이 절대 아니다
+
+}
---
+++ DartStudy/variable.dart
@@ -0,0 +1,14 @@
+void main() {
+  // variable String
+  var name = 'Dart Study';
+
+  print(name);
+
+  var name2 = 'Idle';
+
+  print(name2);
+
+  name = 'not today';
+
+  print(name);
+}
