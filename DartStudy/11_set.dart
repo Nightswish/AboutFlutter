a444542c8f227c307e1ba5e73e9a20f90c460aac
unknown <luvrkdus@gmail.com>
2024-07-24 21:01:46 +0900

    Set

---
+++ DartStudy/11_set.dart
@@ -0,0 +1,17 @@
+void main() {
+  //중복값이 들어갈수 없음. 자등으로 중복 처리함. 
+  final Set<String> names = {
+    'Flutter',
+    'Black Pink',
+  }
+
+  print(names);
+
+  names.add('Jenny');
+  print(names);
+
+  names.remove('Jenny');
+  print(names);
+
+  print(names.contains('Flutter'));
+}
