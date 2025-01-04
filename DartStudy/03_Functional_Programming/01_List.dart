81dd105a4d4ebda858682cc18e232b3597405a41
GGookinWorktree <luvrkdus@gmail.com>
2025-01-04 16:01:28 +0900

    Functional_Programming_Study

---
+++ DartStudy/03_Functional_Programming/01_List.dart
@@ -0,0 +1,19 @@
+void main() {
+  List<String> blackPink = ['로제', '지수', '리사', '제니', '제니'];
+  
+  print(blackPink);
+
+  print(blackPink.asMap());
+  print(blackPink.toSet());
+
+  Map blackPinkMap = blackPink.asMap();
+
+  print(blackPinkMap.keys);
+  print(blackPinkMap.values);
+  print(blackPinkMap.keys.toList());
+  print(blackPinkMap.values.toList());
+
+  Set blackPinkSet = Set.from(blackPink); // Set -> List
+
+  print(blackPinkSet.toList());
+}
